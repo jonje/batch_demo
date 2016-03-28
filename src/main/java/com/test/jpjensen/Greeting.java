2bb1a2ae335338ee3d1fce9bf1b78cf5a6d540e6
Jonathan Jensen <jpjensen@utah.gov>
2016-03-28 14:19:54 -0600

    Add Endpoint hello that returns a greeting

---
+++ src/main/java/com/test/jpjensen/Greeting.java
@@ -0,0 +1,22 @@
+package com.test.jpjensen;
+
+/**
+ * Created by jjensen on 3/28/16.
+ */
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
--- src/main/java/com/test/jpjensen/HelloController.java
+++ src/main/java/com/test/jpjensen/HelloController.java
@@ -1,16 +1,22 @@
 package com.test.jpjensen;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by jjensen on 3/23/16.
  */
-@RestController
+@Controller
+@RequestMapping("/")
 public class HelloController {
 
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    private static final String TEMPLATE = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public @ResponseBody Greeting index(@RequestParam(value="name", required = false, defaultValue = "Stranger") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 }
