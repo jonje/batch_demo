69a4dfec9e26c3d421a87d1ca3817cfd4722b91d
Jonathan Jensen <jpjensen@utah.gov>
2016-03-29 14:52:38 -0600

    Small change

--- src/main/java/com/test/jpjensen/BatchController.java
+++ src/main/java/com/test/jpjensen/BatchController.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobInterruptedException;
-import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
